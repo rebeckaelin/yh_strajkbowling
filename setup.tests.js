f9b1acb2b5794c513471de38d3eeda50f53faa2a
rebeckaelin <rebeckaelin@hotmail.com>
2024-12-09 15:20:03 +0100

    fixing folders

--- setup.tests.js
+++ setup.tests.js
@@ -1,6 +1,6 @@
 import { afterEach, beforeEach } from "vitest";
 import { cleanup } from "@testing-library/react";
-import { server } from "./src/mocks/server";
+import { server } from "./strajk-bowling/src/mocks/server";
 import "@testing-library/jest-dom/vitest";
 
 beforeAll(() => server.listen());
--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     provider: "v8",
     coverage: {
       reportsDirectory: "./coverage",
-      reporter: ["text"],
+      reporter: ["text", "html"],
       all: true,
       include: ["src/**/*.jsx"],
       exclude: [
